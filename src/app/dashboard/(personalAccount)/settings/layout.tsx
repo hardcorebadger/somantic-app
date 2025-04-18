b7faa01db5370ec83d5d7cf07792e03e487d4c37
Jake Trefethen <jake.trefethen@gmail.com>
2025-04-18 16:34:00 -0400

    lint

--- src/app/dashboard/(personalAccount)/settings/layout.tsx
+++ src/app/dashboard/(personalAccount)/settings/layout.tsx
@@ -1,8 +1,9 @@
 import SettingsNavigation from "@/components/dashboard/settings-navigation";
 import DashboardTitle from "@/components/dashboard/dashboard-title";
 import {Separator} from "@/components/ui/separator";
+import React from 'react';
 
-export default function PersonalAccountSettingsPage({children}) {
+export default function PersonalAccountSettingsPage({children}: {children: React.ReactNode}) {
     const items = [
         { name: "Profile", href: "/dashboard/settings" },
         { name: "Teams", href: "/dashboard/settings/teams" },
